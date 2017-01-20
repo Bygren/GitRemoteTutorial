6d0fad45b261e654e68b5ac592f77764cc6d5a1a
Bygren <henrik.bygren@gmail.com>
2017-01-20 18:03:28 +0100

    Sidospår start

--- GitTutorialTest/src/Main.java
+++ GitTutorialTest/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Första ändringen");
 		System.out.println("Andra ändringen");
-
+		System.out.println("-------------");
+		System.out.println("-------------");
 	}
 
 }
